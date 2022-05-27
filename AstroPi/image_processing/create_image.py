a3000ce001a63260409d5b80fb6b1a014c2dd080
Martin Doušek <dousek.martin@gmail.com>
2022-05-27 12:37:23 +0200

    refactoring code

--- AstroPi/image_processing/create_image.py
+++ AstroPi/image_processing/create_image.py
@@ -1,51 +1,53 @@
 import numpy as np
 from PIL import Image
 import csv
+from typing import List
+
 
 
 IMAGE_HEIGHT = 2
 IMAGE_WIDTH = 2
 FRAME_COUNT = 4
+PATH_TO_DATA = '/home/martin/Documents/programování/astropi2021/postpro/Animations/AstroPi/image_processing/pixels.csv'
 
 
 class Photo():
     
-    def __init__(self, name) -> None:
+    def __init__(self, name: str) -> None:
         self.name = name
         self.pixels = np.zeros([2, 2, 3], dtype=np.uint8)
 
     def save(self):
+        """ Saves the image as a file """
         img = Image.fromarray(self.pixels)
         img.save(self.name)
 
 
+def get_data() -> List[List[str]]:
+    """ Extracts data from csv file """
+    with open(PATH_TO_DATA, 'r') as file:
+        reader = csv.reader(file)
+        lines = []
+        for row in reader:
+            lines.append(row)
+            print(row)
+        return lines
 
-with open('/home/martin/Documents/programování/astropi2021/postpro/Animations/AstroPi/image_processing/pixels.csv', 'r') as file:
-    reader = csv.reader(file)
-    lines = []
-    for row in reader:
-        lines.append(row)
-        print(row)
 
-for photo_index in range(FRAME_COUNT):
-    print(photo_index)
-    photo = Photo(f'frame_{photo_index+1}.png')
-    for row in lines[1:]:
-        print(f'Entering row {row[0]} on frame {photo_index+1}')
-        values = row[photo_index+1]
-        print(f"This line contains: _{values}_ with lenght of {len(values)}")
-        index = 0
-        for letter in values:
-            print(f"{index}: _{letter}_")
-            index += 1
-        values = values[1:values.find(')')]
-        values = values.split(', ')
-        print(values)
-        photo.pixels[int(row[0][1]), int(row[0][4])] = values
-
-    photo.save()
-
-# array = np.zeros([100, 200, 3], dtype=np.uint8)
-# array[:,:100] = [255, 128, 0] #Orange left side
-# array[:,100:] = [0, 0, 255]   #Blue right side
-# array[0,0] = [0, 0, 255]
+def create_photos(data: List[List[str]]) -> None:
+    """ Saves multiple photos from list """
+    for photo_index in range(FRAME_COUNT):
+        print(photo_index)
+        photo = Photo(f'frame_{photo_index+1}.png')
+        # ---- SETTING VALUES OF EACH PIXEL ----
+        for row in lines[1:]:
+            # - reformating string rgb values to List -
+            values = row[photo_index+1]
+            values = values[1:values.find(')')]
+            values = values.split(', ')
+            
+            # - setting value of one pixel
+            photo.pixels[int(row[0][1]), int(row[0][4])] = values
+        
+        # ---- SAVING THE IMAGE ----
+        photo.save()
