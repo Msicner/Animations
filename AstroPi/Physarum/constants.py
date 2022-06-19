4b9f68a28e1846dfa2a169253ba9903ebaafff0e
Matěj Šicner <98556500+Msicner@users.noreply.github.com>
2022-06-19 09:26:06 +0200

    Updated paths in constants.py

--- AstroPi/Physarum/constants.py
+++ AstroPi/Physarum/constants.py
@@ -1,9 +1,9 @@
 from load_measurements import load_images_names, load_data, get_average
 
 """ Data constants """
-CSV_FILE_PATH: str = 'C:\\Users\\Matej\\Documents\\Programming\\Projects_git\\Animations\\AstroPi\\Physarum\\vis_map.csv'
-DATA_FOLDER: str = 'C:\\Users\\Matej\\Documents\\Programming\\Projects_git\\Animations\\AstroPi\\measurements_data\\'  # During final testing, change 'testing data' to 'cm_pi'
-SAVE_DATA_FOLDER: str = 'C:\\Users\\Matej\\Documents\\Programming\\Projects_git\\Animations\\AstroPi\\csv_config_data'
+CSV_FILE_PATH: str = #SYSTEM PATH TO YOUR CSV FILE, WHERE THE PROGRAM SHOULD WRITE VISUAL MAP AT THE END
+DATA_FOLDER: str = #SYSTEM PATH TO THE FOLDER WITH MEASUREMENTS DATA AND IMAGES
+SAVE_DATA_FOLDER: str = #SYSTEM PAT TO THE FOLER, WHERE YOU WANT TO STORE CSV DATA FROM IMAGES
 IMAGES_NAMES: list = load_images_names(DATA_FOLDER)
 PHOTO_WEIGHT: float = 0.004
 PHOTO_DURATION: int = 20
@@ -14,7 +14,7 @@ MEASUREMENTS_DATA: dict = load_data(str(DATA_FOLDER + "data.csv"))
 
 """ Constants of cells """
 CELLS: list = []
-NUM_CELLS: int = 120000  # 3 - 15% of the image area
+NUM_CELLS: int = 120000
 
 """ Simulation dimensions """
 SIM_WIDTH: int = 1080
@@ -27,8 +27,8 @@ rotate_angle: int = 45
 """Visual simulation"""
 VSIM_NUM_ROWS: int = SIM_HEIGHT
 VSIM_NUM_COLUMNS: int = SIM_WIDTH
-VSIM_CELL_WEIGHT: float = 0.1#0.03
-VSIM_LIFETIME: float = 9.0  # Describes how many frames you can see the cell in vsim_grid
+VSIM_CELL_WEIGHT: float = 0.1
+VSIM_LIFETIME: float = 9.0
 VSIM_DECAY: float = VSIM_CELL_WEIGHT / VSIM_LIFETIME
 RGB_IN_CSV: bool = False
 
